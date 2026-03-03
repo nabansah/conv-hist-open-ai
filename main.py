626891189a5e723bac1fe945bc99cffe5e0e5692
Nabanita Saha <nabansah.ibm@gmail.com>
2026-03-03 00:11:24 -0600

    initial changes

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+import openai
+
+# load your key from environment instead of hardcoding it
+load_dotenv()
+
+client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+print("Hello, World!")
+
+conversation = []
+
+def chat_with_ai(user_message):
+    conversation.append(
+                {"role": "user", "content": user_message}            
+            )    
+
+    response =  client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                {"role": "system", "content": "You are a helpful assistant."}
+            ] + conversation
+    )  
+
+    ai_answer = response.choices[0].message.content
+
+    conversation.append(
+                {"role": "assistant", "content": ai_answer}            
+            )
+    
+    return ai_answer
+
+
+print(chat_with_ai("what are the symptoms of heart attack"))   
+print("-----")
+print(chat_with_ai("what are the suggested mediacations"))   
+print("-----")
+print(chat_with_ai("please summarize what we just talked"))  
+
+
+
+
